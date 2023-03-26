d57995d66ca3eb1f5f926e6b992ec355ca01f611
Bartlomiej Wojsa <bartlomiej.wojsa@antsolutions.eu>
2023-03-26 20:31:30 +0200

    Initial Commit

---
+++ URLImageViewSwiftUI/URLImageViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLImageViewSwiftUIApp.swift
+//  URLImageViewSwiftUI
+//
+//  Created by Bartłomiej Wojsa on 26/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct URLImageViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
